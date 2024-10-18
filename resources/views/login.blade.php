c5e8977000fde8f1ce0662e0ef7fd5759857da2d
Alessandro L <alessandro.lins@gruponewway.com.br>
2024-10-17 21:58:32 -0300

    007

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,14 +13,27 @@
                 <!-- form -->
                 <div class="row justify-content-center">
                     <div class="col-md-10 col-12">
-                        <form action="#" method="post">
+                        <form action="/loginSubmit" method="post" novalidate>
+                            @csrf
                             <div class="mb-3">
                                 <label for="text_username" class="form-label">Username</label>
-                                <input type="text" class="form-control bg-dark text-info" name="text_username" required>
+                                <input type="text" class="form-control bg-dark text-info" name="text_username" value="{{ old('text_username') }}" required>
+
+                                {{-- show error --}}
+                                @error('text_username')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+
                             </div>
                             <div class="mb-3">
                                 <label for="text_password" class="form-label">Password</label>
-                                <input type="password" class="form-control bg-dark text-info" name="text_password" required>
+                                <input type="password" class="form-control bg-dark text-info" name="text_password"  value="{{ old('text_password') }}" required>
+
+                                {{-- show error --}}
+                                @error('text_password')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+
                             </div>
                             <div class="mb-3">
                                 <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
@@ -33,7 +46,6 @@
                 <div class="text-center text-secondary mt-3">
                     <small>&copy; <?= date('Y') ?> Notes</small>
                 </div>
-
             </div>
         </div>
     </div>
