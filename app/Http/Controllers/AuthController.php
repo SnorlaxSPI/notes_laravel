3cd4c2658abee9edf1234c5e3c045a192067c5fe
Alessandro L <alessandro.lins@gruponewway.com.br>
2024-10-19 17:38:16 -0300

    007

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Stmt\TryCatch;
 
 class AuthController extends Controller
 {
@@ -31,7 +33,14 @@ class AuthController extends Controller
             $username = $request->input('text_username');
             $password = $request->input('text_password');
 
-            echo 'OK!';
+            // test dababase connection
+            try {
+                DB::connection()->getPdo();
+                echo 'Connection is OK!';
+            } catch (\PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+            echo "FIM!";
     }
 
     public function logout() {
