faa9df19f1402027793309f14338e4b9295e3071
Alessandro L <alessandro.lins@gruponewway.com.br>
2024-10-23 19:24:27 -0300

    008

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use PhpParser\Node\Stmt\TryCatch;
@@ -30,17 +31,43 @@ class AuthController extends Controller
         );
 
         //get user input
-            $username = $request->input('text_username');
-            $password = $request->input('text_password');
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
 
-            // test dababase connection
-            try {
-                DB::connection()->getPdo();
-                echo 'Connection is OK!';
-            } catch (\PDOException $e) {
-                echo "Connection failed: " . $e->getMessage();
-            }
-            echo "FIM!";
+        // check if user exists
+        $user = User::where('username', $username)
+                      ->where('deleted_at', NULL)
+                      ->first();
+        echo '<pre>';
+        print_r($user);
+        //if(!$user) {
+        //    return redirect()
+        //           ->back()
+        //           ->withInput()
+        //           ->with('loginErr', 'Username ou password incorretos.');
+        //}
+//
+        //// check if password is correct
+        //if(!password_verify($password, $user->password)) {
+        //    return redirect()
+        //           ->back()
+        //           ->withInput()
+        //           ->with('loginErr', 'Username ou password incorretos.');
+        //}
+//
+        //// update last login
+        //$user->last_login = date('Y-m-d H:i:s');
+        //$user->save();
+//
+        //// login user
+        //session([
+        //    'user'=> [
+        //        'id' => $user->id,
+        //        'username' => $user->username
+        //        ]
+        //    ]);
+//
+        //    echo 'LOGIN COM SUCESSO';
     }
 
     public function logout() {
