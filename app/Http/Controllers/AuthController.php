6065eb32ef1942178fe1af11fc47c497e9f9d2d1
Alessandro L <alessandro.lins@gruponewway.com.br>
2024-10-26 16:55:36 -0300

    010 - Criar a pagina principal

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,7 +65,8 @@ class AuthController extends Controller
             ]
         ]);
 
-        echo 'LOGIN COM SUCESSO';
+        // redirect to home
+        return redirect()->to('/');
     }
 
     public function logout() {
