8908ef9e465ef8d85debc5c596db7ab772d23e06
Alessandro L <alessandro.lins@gruponewway.com.br>
2024-11-09 19:36:42 -0300

    013 - Preparando a pagina

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,7 +20,12 @@ class MainController extends Controller
     }
 
     public function newNote() {
-        echo "Im creating a new note!";
+       // show new note view
+       return view('new_note');
+    }
+
+    public function newNoteSubmit(Request $request) {
+        echo "Im creating a new note.";
     }
 
     public function editNote($id) {
